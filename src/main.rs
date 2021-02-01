0f79aa96689913abb19e6aaf6c860cedd33405d4
captain-yossarian <sergiybiluk@gmail.com>
2021-02-01 22:45:31 +0100

    update cors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
-extern crate iron;
+use actix_cors::Cors;
+use actix_web::{get, http, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
-use iron::prelude::*;
-use iron::status;
+#[derive(Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+}
 
-fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
 
-    let _server = Iron::new(hello_world).http("0.0.0.0:8080").unwrap();
-    println!("On 8080");
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().json(MyObj {
+        name: "John Doe".to_string(),
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("https://localhost:3000")
+            .allowed_methods(vec!["GET"])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
+        App::new()
+            .service(hello)
+            .service(echo)
+            .wrap(cors)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
